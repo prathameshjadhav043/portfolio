783fdd0abab8a951eb02da7645439b06434ba70f
Prathamesh <prathameshjadhav043@gmail.com>
2023-02-01 21:47:02 +0530

    feat: material ui add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Portfolio</h1>
+    <div>
+      <Button variant="contained">Hello World</Button>
     </div>
   );
 }
