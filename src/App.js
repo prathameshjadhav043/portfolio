7470c9efb6760d3848e6f356ae1295412d7c2397
Prathamesh <prathameshjadhav043@gmail.com>
2023-02-01 22:19:15 +0530

    feat: mi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import Navbar from './components/AppBar/Navbar';
 
 function App() {
   return (
+    <>
+      <Navbar />
     <div>
-      <Button variant="contained">Hello World</Button>
+        {/* <Button variant="contained">Hello World</Button> */}
     </div>
+    </>
   );
 }
 
